4f3a660b2dd5223a049f9b02f91f1d598408b7fb
Caetanocc <58012552+Caetanocc@users.noreply.github.com>
2023-07-06 22:04:22 -0300

    Add files via upload

--- ju/app.js
+++ ju/app.js
@@ -14,9 +14,10 @@ function validarCodigo(btn) {
     let senha 
     let pagina
 
-    if (btn == "btn0") { senha = '731128'; pagina = "index2.html" }
-    if (btn == "btn1") { senha = '080702'; pagina = "index3.html" }
+    if (btn == "btn0") { senha = '120573'; pagina = "index2.html" }
+    if (btn == "btn1") { senha = '0807'  ; pagina = "index3.html" }
     if (btn == "btn2") { senha = '51'    ; pagina = "index4.html" }
+    if (btn == "btn3") { senha = '1000'  ; pagina = "index5.html" }
 
     // Verifica se o código é numérico
     if (!isNaN(codigo) && codigo == senha ) {
