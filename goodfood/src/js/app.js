bb7ec0a7dbfdebd7875608491657d0c7d5d8aa91
Caetanocc <claudiomiro.silva2@etec.sp.gov.br>
2023-10-29 18:39:46 -0300

    app

--- goodfood/src/js/app.js
+++ goodfood/src/js/app.js
@@ -1,5 +1,6 @@
 let notaModal = document.querySelector('#notaModal')
 let bodyModalDados = document.querySelector('#bodyModalDados')
+let ModalPedidos = document.querySelector('#ModalPedidos')
 
 function CarrinhosDeCompras() {
     const url = `https://etec22s2-default-rtdb.firebaseio.com/goodfood/${mesa}.json`;
@@ -51,7 +52,57 @@ function CarrinhosDeCompras() {
        
 }
 
+function Pedidos() {
+    const url = `https://etec22s2-default-rtdb.firebaseio.com/goodfood/${mesa}.json`;
+    console.log(mesa)
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    };
 
+    fetch(url, options)
+        .then(response => response.json())
+        .then(dados => {
+            if (dados.error) {
+                linha = '<h5> Erro ao consultar BD</h5>';
+                bodyModalDados.innerHTML = linha;
+                return;
+            }
+
+            if (dados) {
+                console.log(dados);
+                let tabela = document.getElementById('tabelaPedidos');
+                let tbody = tabela.querySelector('tbody');
+
+                tbody.innerHTML = '';
+                
+                for (let chave in dados) {
+                    let item = dados[chave];
+                    if(item.status == 2){
+                      
+                        let row = document.createElement('tr');
+                        row.innerHTML = `
+                            <td>${item.desc}</td>
+                            <td>${item.qtde}</td>
+                            <td>${item.valor}</td>
+                        `;
+                        tbody.appendChild(row);
+                    }
+                  
+                }
+            
+            } else {
+                // Handle the case where there are no data
+            }
+        });
+
+       
+}
+
+ModalPedidos.addEventListener('focus', Pedidos)
 
 buttonadicionar = document.getElementById('carrinhoEnviar');
 buttonadicionar.addEventListener('click',function atualizarItem(){
