a468e519f986271f3aef17e97319dfb389ca6ed1
João Felipe <85209912+J0A0F3L1P3@users.noreply.github.com>
2024-04-04 19:12:27 -0300

    Update insJsAlunos.js

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -3,3 +3,8 @@ function ALUNO() {
     linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
 
+function JOAO_FELIPE() {
+    linkGit = document.querySelector("#JOAO_FELIPE")
+    linkGit.innerText = "https://github.com/J0A0F3L1P3/mini-portfolio"
+}
+
