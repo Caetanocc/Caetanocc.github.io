9120f3d138050bd6972286a06a34c7d54df7d1a8
Lucas Rafael Lima da Silva <133080973+luellima@users.noreply.github.com>
2024-04-04 19:58:07 -0300

    Update insJsAlunos.js

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -3,3 +3,12 @@ function ALUNO() {
     linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
 
+function LUCAS_RAFAEL() {
+    linkGit = document.querySelector("#LUCAS_RAFAEL")
+    linkGit.innerText = "https://luellima.github.io/perfil/"
+}
+
+function LUIZ() {
+    linkGit = document.querySelector("#LUIZ")
+    linkGit.innerText = "https://luizvictorino.github.io/perfil/"
+}
