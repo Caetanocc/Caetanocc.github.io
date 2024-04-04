b214a54e7fee2fa7cf8599c46fa2b43c1bebd2af
Lucas Gomes <lucas.gomes247@etec.sp.gov.br>
2024-04-04 20:17:07 -0300

    Lucas Gomes Changes

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -1,21 +1,25 @@
 function ALUNO() {
-    linkGit = document.querySelector("#ALUNO")
-    linkGit.innerText = "https://caetanocc.github.io/perfil"
+  linkGit = document.querySelector("#ALUNO")
+  linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
 
 function JOAO_MIGUEL() {
-    linkGit = document.querySelector("#JOAO_MIGUEL")
-    linkGit.innerHTML = '<a href="https://github.com/Jhonnjust">https://github.com/Jhonnjust</a>';
+  linkGit = document.querySelector("#JOAO_MIGUEL")
+  linkGit.innerHTML = '<a href="https://github.com/Jhonnjust">https://github.com/Jhonnjust</a>';
 }
 
 
 function LUCAS_BARROS() {
-    linkGit = document.querySelector("#LUCAS_BARROS")
-    linkGit.innerText = "https://github.com/LulzUser/LulzUser.git"
+  linkGit = document.querySelector("#LUCAS_BARROS")
+  linkGit.innerText = "https://github.com/LulzUser/LulzUser.git"
 }
 
 function JOAO_FELIPE() {
-    linkGit = document.querySelector("#JOAO_FELIPE")
-    linkGit.innerText = "https://github.com/J0A0F3L1P3/mini-portfolio"
+  linkGit = document.querySelector("#JOAO_FELIPE")
+  linkGit.innerText = "https://github.com/J0A0F3L1P3/mini-portfolio"
 }
 
+function LUCAS_GOMES() {
+  linkGit = document.querySelector('#LUCAS_GOMES');
+  linkGit.innerText = 'https://lucas-dos-santos-gomes.github.io/mini-portfolio/';
+}
