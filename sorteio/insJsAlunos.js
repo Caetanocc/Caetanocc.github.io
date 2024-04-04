898b0e5c4a911f6a3994c75ddbcdd2d9f90c9356
Lucas Barros Gouveia <160260364+LulzUser@users.noreply.github.com>
2024-04-04 19:25:31 -0300

    Update insJsAlunos.js

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -3,3 +3,7 @@ function ALUNO() {
     linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
 
+function LUCAS_BARROS() {
+    linkGit = document.querySelector("#LUCAS_BARROS")
+    linkGit.innerText = "https://github.com/LulzUser/LulzUser.git"
+}
