282a00d2ad2f574db9231ec891f7910bcfaf263c
Gugagla <gugdla@gmail.com>
2024-04-04 20:05:26 -0300

    Link de Gustavo Gonçalves

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -8,3 +8,8 @@ function JOAO_FELIPE() {
     linkGit.innerText = "https://github.com/J0A0F3L1P3/mini-portfolio"
 }
 
+function GUSTAVO_GONCALVES() {
+    linkGit = document.querySelector("#GUSTAVO_GONCALVES")
+    linkGit.innerText = "https://github.com/Gugagla/Gugagla"
+}
+
