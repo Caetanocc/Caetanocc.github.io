29b53a630893211f571cac79d7496e58b4abbf27
Jhonnjust <160186972+Jhonnjust@users.noreply.github.com>
2024-04-04 19:29:05 -0300

    Update insJsAlunos.js

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -1,3 +1,8 @@
+function ALUNO() {
+    linkGit = document.querySelector("#ALUNO")
+    linkGit.innerText = "https://caetanocc.github.io/perfil"
+}
+
 function JOAO_MIGUEL() {
     linkGit = document.querySelector("#JOAO_MIGUEL")
     linkGit.innerText = "https://github.com/Jhonnjust"
