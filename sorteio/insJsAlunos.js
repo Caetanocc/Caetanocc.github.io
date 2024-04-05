eb5a110ab8a93dac7293bb7d6a4d68c3dca3c562
OliveiraCrl <160975410+OliveiraCrl@users.noreply.github.com>
2024-04-04 21:44:13 -0300

    Update insJsAlunos.js

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -120,7 +120,7 @@ function VINICIUS_TAVARES() {
     linkGit.innerHTML = '<a href="https://tavaresvinicius.github.io/perfil/">https://tavaresvinicius.github.io/perfil/</a>'
 }
 
-function GUSTAVO_SOARES() {
-    linkGit = document.querySelector("#GUSTAVO_SOARES")
-    linkGit.innerText = '<a href="https://github.com/Shockraa">https://github.com/Shockraa</a>'
-  }
+function CARLOS() {
+    linkGit = document.querySelector("#CARLOS")
+    linkGit.innerHTML = '<a href="https://github.com/OliveiraCrl">https://github.com/OliveiraCrl</a>'
+}
