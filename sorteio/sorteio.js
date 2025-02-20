f1f72d16a44fbbcc939bbeb9f87140923bde26d0
Aluno <etecae023@hotmail.com>
2025-02-20 20:03:34 -0300

    Eu

--- sorteio/sorteio.js
+++ sorteio/sorteio.js
@@ -1,8 +1,10 @@
+/* https://drive.google.com/file/d/0B9eZlIWAs3-sN3NRbktQNVFUN3l2cTBBcXN4Y3FaUQ/view?resourcekey=0-ZafqCRtyIP8Zw0CKviW5Gw */
+
 const alunos = 
 [
-  'ALUNO'   
- ,'AGHATA' 
- 
+  'ALUNO',
+  'AGHATA',
+  'LEONARDO'
 ]
 
 // alunos.sort()
