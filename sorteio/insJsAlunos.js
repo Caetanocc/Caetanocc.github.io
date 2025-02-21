8d69714d49c2e8a2eef458e06dbe4dd012758111
Aluno <etecae023@hotmail.com>
2025-02-20 21:43:43 -0300

    OI

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -3,7 +3,7 @@ function ALUNO() {
   linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
 function FELIPE() {
-  linkGit = document.querySelector("#ALUNO")
+  linkGit = document.querySelector("#FELIPE")
   linkGit.innerText = "https://www.google.com/search?q=friv&sca_esv=9632110e93a89f4a&udm=2&biw=1280&bih=585&ei=Jbe3Z4SmHpDb1sQP1Jex8Qo&ved=0ahUKEwjEzN-ysNOLAxWQrZUCHdRLLK4Q4dUDCBI&uact=5&oq=friv&gs_lp=EgNpbWciBGZyaXYyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMggQABiABBixAzIFEAAYgAQyCBAAGIAEGLEDMggQABiABBixAzIEEAAYAzIFEAAYgARItRZQAFioFHAAeACQAQCYAWOgAc8DqgEBNbgBA8gBAPgBAZgCBKACjgOoAgDCAgoQABiABBhDGIoFwgIMEAAYgAQYQxiKBRgKwgINEAAYgAQYsQMYQxiKBZgDVZIHAzEuM6AH_RM&sclient=img"
 }
 
