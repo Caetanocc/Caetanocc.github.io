1ab716f3b3e9813c4896685395bb8189485257aa
Aluno <etecae023@hotmail.com>
2025-02-20 21:43:46 -0300

    oi

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -2,4 +2,7 @@ function ALUNO() {
   linkGit = document.querySelector("#ALUNO")
   linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
-
+function RAFFAELA(){
+  linkGit = document.querySelector("#RAFFAELA")
+  linkGit.innerHTML = "https://www.java.com/pt-BR/download/help/whatis_java.html"
+}
