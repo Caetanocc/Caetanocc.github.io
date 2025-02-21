f69b6f58a532e38a6fded089591c89cee7283e3e
GabiszSantos <gabisz20202@gmail.com>
2025-02-20 21:42:33 -0300

    commit

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -2,4 +2,11 @@ function ALUNO() {
   linkGit = document.querySelector("#ALUNO")
   linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
-
+function GABRIEL_ISCHIAVOLINI() {
+  linkGit = document.querySelector("#GABRIEL_ISCHIAVOLINI")
+  linkGit.innerText = "https://github.com/Bielmonge"
+}
+function GABRIEL_SANTOS() {
+  linkGit = document.querySelector("#GABRIEL_SANTOS")
+  linkGit.innerText = "https://github.com/GabiszSantos"
+}
