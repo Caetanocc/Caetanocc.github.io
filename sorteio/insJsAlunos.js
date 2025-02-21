fd198fb18d4928ad12dbd8916bffebe129538b34
Aluno <etecae023@hotmail.com>
2025-02-20 21:39:45 -0300

    Add link

--- sorteio/insJsAlunos.js
+++ sorteio/insJsAlunos.js
@@ -2,4 +2,8 @@ function ALUNO() {
   linkGit = document.querySelector("#ALUNO")
   linkGit.innerText = "https://caetanocc.github.io/perfil"
 }
+function FABRICIO() {
+  linkGit = document.querySelector("#FABRICIO")
+  linkGit.innerText = "https://www.youtube.com/watch?v=LVE17tSApHI"
+}
 
