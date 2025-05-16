144d8255af29bbc4f5759b590a2961e84c2c4e5d
Caetanocc <58012552+Caetanocc@users.noreply.github.com>
2025-05-15 21:37:51 -0300

    Update fbconfig.js

--- js/fbconfig.js
+++ js/fbconfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCaB0vLOOvCjMim9Z-UfK4W2ryvD-15tH4",
-  authDomain: "etec24-3dc8c.firebaseapp.com",
-  databaseURL: "https://etec24-3dc8c-default-rtdb.firebaseio.com",
-  projectId: "etec24-3dc8c",
-  storageBucket: "etec24-3dc8c.appspot.com",
-  messagingSenderId: "452717358267",
-  appId: "1:452717358267:web:76df1c741b160aec118d9e"
+  apiKey: "--",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "etec24-om",
+  messagingSenderId: "",
+  appId: "1:452c118d9e"
   
 };
   
