f27cb919960b8b3a05936cb66a744850b25bda1a
caetano1973 <caetano19731128@gmail.com>
2025-08-05 18:43:58 -0300

    atualizar lista.

--- sorteio/sorteio.js
+++ sorteio/sorteio.js
@@ -1,39 +1,36 @@
-/* https://drive.google.com/file/d/0B9eZlIWAs3-sN3NRbktQNVFUN3l2cTBBcXN4Y3FaUQ/view?resourcekey=0-ZafqCRtyIP8Zw0CKviW5Gw */
 
 const alunos = 
 [
-
- 'ALUNO'   
-,'AGHATA'   
-,'ANGELO'   
-,'CAIQUE'   
-,'DIEGO'   
-,'EDUARDO'  
-,'FABRICIO' 
-,'FELIPE' 
-,'GABRIEL_SANTOS'
-,'GABRIEL_ALMEIDA'
-,'GABRIEL_MENDES'
-,'GABRIEL_ISCHIAVOLINI'
-,'GIANLUCA'
-,'GUILHERME_AZEVEDO'
-,'GUILHERME_LIMA'
-,'GUSTAVO_FERREIRA'
-,'GUSTAVO_NEVES'
-,'GUSTAVO_PEREIRA'
-,'GUSTAVO_TORELLI'
-,'JUAN'
-,'KAUE'
-,'LEONARDO'
-,'LUIS'
-,'PEDRO_VIEIRA'
-,'PEDRO_NUNES'
-,'RAFAEL'
-,'RAFFAELA'
-,'RONALDO'
-,'VICTOR'
- ]
-
+  'ALUNO',   
+  "ANGELO",
+  "ARTHUR",
+  "CAIO",
+  "CAIQUE",
+  "EDUARDO",
+  "ENZO",
+  "FERNANDO",
+  "FRANCISCO",
+  "GABRIEL",
+  "GUILHERME",
+  "GUSTAVO",
+  "INGRID",
+  "IVY",
+  "JHONNY",
+  "JONATHAN",
+  "JONATHAS",
+  "JULIO",
+  "KAUE_BUENO",
+  "KAUE_DAVI",
+  "LUCAS_HENRIQUE",
+  "LUCAS_TEIXEIRA",
+  "MARCIANO",
+  "MATHEUS",
+  "NICOLAS",
+  "OLIVER",
+  "RODRIGO",
+  "ROGER",
+  "VITOR"
+];
 // alunos.sort()
 
 let text = '<table id="t01"> ';
