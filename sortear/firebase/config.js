c9a7dcbd154f843a20be1b2d43b8f5eba3310943
Caetanocc <58012552+Caetanocc@users.noreply.github.com>
2025-08-05 18:28:39 -0300

    Update config.js

--- sortear/firebase/config.js
+++ sortear/firebase/config.js
@@ -2,13 +2,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaB0vLOOvCjMim9Z-UfK4W2ryvD-15tH4",
-    authDomain: "etec24-3dc8c.firebaseapp.com",
-    databaseURL: "https://etec24-3dc8c-default-rtdb.firebaseio.com",
-    projectId: "etec24-3dc8c",
-    storageBucket: "etec24-3dc8c.appspot.com",
-    messagingSenderId: "452717358267",
-    appId: "1:452717358267:web:76df1c741b160aec118d9e"
+    apiKey: "--15tH4",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "e"
 
     
 };
