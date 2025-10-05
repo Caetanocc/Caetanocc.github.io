ca3fa650bf0e0f55799d0849248589c178d3bfe0
Caetanocc <58012552+Caetanocc@users.noreply.github.com>
2025-10-05 18:20:57 -0300

    Update fbconfig.js

--- js/fbconfig.js
+++ js/fbconfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
 
-  apiKey: "--",
-  authDomain: "m",
-  databaseURL: "https://com",
-  projectId: "ec",
-  storageBucket: "et",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCaB0vLOOvCjMim9Z-UfK4W2ryvD-15tH4",
+  authDomain: "etec24-3dc8c.firebaseapp.com",
+  databaseURL: "https://etec24-3dc8c-default-rtdb.firebaseio.com",
+  projectId: "etec24-3dc8c",
+  storageBucket: "etec24-3dc8c.firebasestorage.app",
+  messagingSenderId: "452717358267",
+  appId: "1:452717358267:web:76df1c741b160aec118d9e"
   
 };
   
@@ -17,3 +17,4 @@ const app = firebase.initializeApp(firebaseConfig);
 const dbRef = firebase.database().ref();
 const pessoaRef = dbRef.child('game');
   
+
